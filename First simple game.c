4d4f0493a5b361faf138aca63878ff3491c891bb
Marshall <marshall_work@mail.ru>
2021-06-23 23:56:38 +0300

    Implemented a modular architecture

--- First simple game.c
+++ First simple game.c
@@ -5,65 +5,96 @@
 #include <conio.h>      //дл€ getch()
 #include <stdlib.h>		//дл€ rand() и RAND_MAX
 
+#define DOG_START_X 10
+#define DOG_START_Y 10
+
+char screen[20][31];       //игровое поле
+int score = -1;            //счет (-1 инкрементируетс€ в 0 при первой генерации координат цели)
+typedef struct {       //Unit - объект на игровом поле
+		int x;
+		int y;
+}Unit;
+
+Unit Dog = {DOG_START_X, DOG_START_Y};				//герой
+Unit BfrDog;               							//предыдущее положение геро€
+Unit Purpose = {DOG_START_X, DOG_START_Y};			//цель			
+	
+void InitMap(void); 
+void SetDog(void);	
+void SetPurpose(void);	
+void ShowMap(void);	
+	
+char key;					//нажата€ клавиша
+int i;	
+			
 int main(void){
 	setlocale(LC_ALL, "Rus");
-	char screen[20][31];       //игровое поле
-	int x, y;
-	int score = -1;				//счет (-1 инкрементируетс€ в 0 при первой генерации координат цели)
-	int xDog = 10, 				//текущиемкоординаты геро€
-		yDog = 10,				//
-		bfr_xDog,				//предыдущие координаты геро€
-		bfr_yDog,				//
-		xPurpose = xDog,		//координаты цели (при первой инициализации равны координатам 
-		yPurpose = yDog;		//геро€ дл€ правильного запуска их последующей генерации)
-	char key;	
-	int i;				//нажата€ клавиша
-	
 	do{
-		/*»нициализаци€ границ игрового пол€*/
-		sprintf(screen[0], "#############################");          
-		for(i = 1; i<20; i++){
-			sprintf(screen[i], "#                           #");
-		}
-		sprintf(screen[19], "#############################");
-		
-		/*«адание координат геро€*/
-		bfr_xDog = xDog;
-		bfr_yDog = yDog;
-		if(key == 'w') xDog--;
-		if(key == 's') xDog++;
-		if(key == 'a') yDog--;
-		if(key == 'd') yDog++;
-		if(screen[xDog][yDog] == '#'){
-			xDog = bfr_xDog;
-			yDog = bfr_yDog;
-		}
-		
-		screen[xDog][yDog] = '@';
-		
-		/*«адание координат цели*/
-		if(xPurpose == xDog && yPurpose == yDog){
-			score++;
-			srand(time(NULL));
-			xPurpose = rand()%18;
-			yPurpose = rand()%28;
-			if(xPurpose == 0) xPurpose++;
-			if(yPurpose == 0) yPurpose++;
-		}
-		
-		screen[xPurpose][yPurpose] = '*';
+		InitMap();
+		SetDog();
+		SetPurpose();
 		
 		system("cls");
 		
-		/*¬ывод игрового пол€ на экран*/
-		for(i = 0; i < 20; i++)									
-			  printf("%s\n", screen[i]);
-  			  printf("SCORE: %d", score);
-  			  key = getch();
+		ShowMap();
+		
+	 	key = getch();
 	} while(key  != 'q');
 	
 
 	printf("\n**********GAME OVER**********\n");
 		
 	return 0;
+}
+
+
+/*»нициализаци€ игрового пол€*/
+void InitMap()
+{
+	sprintf(screen[0], "#############################");          
+		for(i = 1; i<20; i++){
+			sprintf(screen[i], "#                           #");
+		}
+	sprintf(screen[19], "#############################");
+}
+
+
+/*«адание координат геро€*/
+void SetDog()
+{
+	BfrDog.x = Dog.x;
+	BfrDog.y = Dog.y;
+	if(key == 'w') Dog.x--;
+	if(key == 's') Dog.x++;
+	if(key == 'a') Dog.y--;
+	if(key == 'd') Dog.y++;
+	if(screen[Dog.x][Dog.x] == '#'){
+		Dog.x = BfrDog.x;
+		Dog.y = BfrDog.y;
+	}
+	screen[Dog.x][Dog.y] = '@';
+}
+
+
+/*«адание координат цели*/
+void SetPurpose()
+{
+	if(Purpose.x == Dog.x && Purpose.y == Dog.y){
+			score++;
+			srand(time(NULL));
+			Purpose.x = rand()%18;
+			Purpose.y = rand()%28;
+			if(Purpose.x == 0) Purpose.x++;
+			if(Purpose.y == 0) Purpose.y++;
+	}
+	screen[Purpose.x][Purpose.y] = '*';
+}
+
+
+/*ќтображение игрового пол€ на экране*/
+void ShowMap()
+{
+ 	 for(i = 0; i < 20; i++)									
+		 printf("%s\n", screen[i]);
+	printf("SCORE: %d", score);
 }
